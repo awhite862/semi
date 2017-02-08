391f5c70d4aceb2421ccad71d811c8f8a699662e
Alec White <whiteaf@berkeley.edu>
2017-02-08 13:04:44 -0800

    I/O capabilities in main.cpp

--- src/io/io_input_section.cpp
+++ src/io/io_input_section.cpp
@@ -6,7 +6,6 @@
 //
 #include "io_input_section.h"
 #include <iomanip>
-#include <sstream>
 #include "io_utils.h"
 
 namespace io {
@@ -17,22 +16,50 @@ void input_section::add(std::string &key, std::string &value) {
     m_data[key] = value;
 }
 
-void input_section::add(std::string &input) {
-    clean_string(input);
-    std::stringstream ss;
-    std::string key, value;
-    ss << input;
-    ss >> key; ss >> value;
-    if (!key.empty() && !value.empty()) {
-        m_data[key] = value;
+void input_section::print(std::ostream &os) const {
+    for (typename map_type::const_iterator ii = m_data.begin();
+        ii != m_data.end(); ++ii) {
+
+        os << " " << ii->first << " " << ii->second << std::endl;
+    }
+}
+    
+bool input_section::is_empty() const {return m_data.empty();}
+
+void get_input_sections(
+    std::istream &is, 
+    std::map<std::string, input_section> &input) {
+
+    std::string name;
+    while ( std::getline(is, name) ) {
+        clean_string(name);
+        if (!name.empty() && name[0] == '$') {
+            name = name.substr(1,name.length());
+            input[name] = input_section();
+            std::string temp;
+            while ( std::getline(is, temp) ) {
+                clean_string(temp);
+                if (!temp.empty() && temp[0] == '$') break;
+                std::string key, value;
+                std::stringstream ss;
+                if (!temp.empty()) {
+                    ss << temp;
+                    ss >> key; ss >> value;
+                    input[name].add(key, value);
+                }
+            }
+        }
     }
 }
 
-void input_section::print(std::ostream &os) {
-    for (typename map_type::iterator ii = m_data.begin();
-        ii != m_data.end(); ++ii) {
+void print_input_sections(
+    std::ostream &os, 
+    std::map<std::string, input_section> &input) {
 
-        os << ii->first << " " << ii->second << std::endl;
+    typedef std::map<std::string, input_section>::iterator itype;
+    for (itype ii = input.begin(); ii != input.end(); ++ii) {
+        os << ii->first << ": " << std::endl;
+        ii->second.print(os);
     }
 }
 
--- src/io/io_input_section.h
+++ src/io/io_input_section.h
@@ -10,6 +10,7 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 namespace io {
 
@@ -22,9 +23,30 @@ private:
 public:
     input_section();
     void add(std::string &key, std::string &value);
-    void add(std::string &input);
-    void print(std::ostream &os);
+    void print(std::ostream &os) const;
+    bool is_empty() const;
+    
+    template<typename T>
+    T get_value(const std::string &key) {
+        if(m_data.count(key) == 0) {
+            throw std::logic_error(
+                "input_section::get_value(): parameter not found " + key);
+        }
+        const std::string val = m_data[key];
+        T out;
+        std::stringstream ss;
+        ss << val; ss >> out;
+        return out;
+    }
 };
+
+void get_input_sections(
+    std::istream &is, 
+    std::map<std::string, input_section> &input);
+
+void print_input_sections(
+    std::ostream &os, 
+    std::map<std::string, input_section> &input);
 
 } // namespace io 
 
