ad5fedcab3663e7b86d666b405fc35a5ea20d2c6
Alec White <whiteaf@berkeley.edu>
2017-02-09 12:50:23 -0800

    track .gitignore, CMakeLists.txt in src/semi

---
+++ src/semi/test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+void test() {
+    int i = 1;
+    int j = i + 1;
+}
---
+++ src/semi/test.h
@@ -0,0 +1,11 @@
+#ifndef SEMI_TEST_H
+#define SEMI_TEST_H
+
+namespace semi {
+
+void test();
+
+} // namespace semi
+
+#endif // SEMI_TEST_H
+
